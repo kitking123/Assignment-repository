6ec5aea2456d84c342da9c611b563c6140635e0a
kitking123 <amk0013@naver.com>
2022-10-12 22:29:58 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,8 @@
-def print_hello():
-    print("Hello World!!")
-
-if __name__ == '__main__':
-    print_hello()
-
 
 def print_bye():
     print("Bye World T_T")
+
+def Number():
+    a = 1
+    b = 2
+    print(a+b)
